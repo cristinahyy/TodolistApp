9b9f0b8b4aab81aec893ed3786770a5e39339c39
crist <cristina.hu@edu.unifi.it>
2025-12-03 17:50:57 +0100

    Unit Test

--- test/ActivityTest.cpp
+++ test/ActivityTest.cpp
@@ -1,5 +1,14 @@
 #include "gtest/gtest.h"
-
 #include "../Activity.h"
 
+TEST(ActivityTest, Initialization) {
+    Activity task("Fare la spesa");
+    EXPECT_EQ(task.getDescription(), "Fare la spesa");
+    EXPECT_FALSE(task.isCompleted());
+}
 
+TEST(ActivityTest, ToggleStatus) {
+    Activity task("Studiare C++");
+    task.setCompleted(true);
+    EXPECT_TRUE(task.isCompleted());
+}
--- test/TodolistTest.cpp
+++ test/TodolistTest.cpp
@@ -1,5 +1,32 @@
 #include "gtest/gtest.h"
+#include "../Todolist.h"
 
-#include "../Activity.h"
+TEST(TodolistTest, AddAndRemove) {
+    Todolist manager;
+    manager.addActivity("Test 1");
+    manager.addActivity("Test 2");
 
+    auto list = manager.getActivities();
+    EXPECT_EQ(list.size(), 2);
+    EXPECT_EQ(list[0].getDescription(), "Test 1");
 
+    manager.removeActivity(0);
+    EXPECT_EQ(manager.getActivities().size(), 1);
+    EXPECT_EQ(manager.getActivities()[0].getDescription(), "Test 2");
+}
+
+TEST(TodolistTest, Persistence) {
+    Todolist manager;
+    manager.addActivity("Save Me");
+    manager.getActivities()[0].setCompleted(true);
+
+    std::string testFile = "test_data.txt";
+    manager.saveToFile(testFile);
+
+    Todolist newManager;
+    newManager.loadFromFile(testFile);
+
+    auto list = newManager.getActivities();
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list[0].getDescription(), "Save Me");
+}
