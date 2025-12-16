6ed55302dfdc90741eff12695feb12f507f701fe
crist <cristina.hu@edu.unifi.it>
2025-12-16 08:51:16 +0100

    fix

--- Todolist.h
+++ Todolist.h
@@ -7,14 +7,13 @@
 
 class Todolist {
 public:
-    void addActivity(const std::string& description);
+    void addActivity(const std::string& desc, const std::string& date);
     void removeActivity(int index);
     void markCompleted(int index);
     std::vector<Activity> getActivities() const;
 
     void saveToFile(const std::string& filename);
     void loadFromFile(const std::string& filename);
-    void addActivity(const std::string& desc, const std::string& date);
 
 private:
     std::vector<Activity> activities;
